0605a2b411ea36ec222c30d42c2e82b90fe530e9
Jonathan Yale <jonathanyale@gmail.com>
2020-04-15 14:55:00 -0400

    cleaned up console.log, etc

--- intermediate-algorithm-scripting/convert-html-entities.js
+++ intermediate-algorithm-scripting/convert-html-entities.js
@@ -6,47 +6,21 @@ const entities = {
   "'": '&apos;'
 };
 
-// NOTE: this implementation relies on a global variable
-// instead have entities as parameter
-// user should pass an object with all entities
-// didn't do becasue I don't want to break fCC testing
-
-// console.log(entities['&']);
-
 function convertHTML(str) {
   const arr = str.split('');
-  // console.log(arr);
-  // console.log(Object.keys(entities)[0]);
-  // console.log(arr[6]);
-  // console.log(Object.keys(entities).includes(arr[6]));
-  // console.log(entities[arr[6]]);
   for (let i = 0; i < arr.length; i++) {
     if (Object.keys(entities).includes(arr[i])) {
       str = str.replace(arr[i], entities[arr[i]])
     }
   }
-  console.log(str);
   return str;
 }
 
 convertHTML("Dolce & Gabbana");
+convertHTML("Hamburgers < Pizza < Tacos");
+convertHTML("Sixty > twelve");
 
-
-console.log();
-console.log();
-console.log();
-console.log();
-console.log();
-
-
-// & = &amp;
-// < = &lt;
-// > = &gt;
-// " = &quot;
-// ' = &#39;
-
-// &amp;
-// &lt;
-// &gt;
-// &quot;
-// &#039;
+// NOTE: this implementation relies on a global variable
+// instead have entities as a parameter
+// user should pass an object with all entities
+// didn't do because didn't want to break fCC testing

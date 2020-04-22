2756e2984c913d4fc6cc9eada7cfba9d38512e0a
Jonathan Yale <jonathanyale@gmail.com>
2020-04-22 14:43:03 -0400

    cleaned up. Added test cases at bottom

--- intermediate-algorithm-scripting/pig-latin.js
+++ intermediate-algorithm-scripting/pig-latin.js
@@ -1,18 +1,11 @@
 function translatePigLatin(str) {
-  // console.log(str[0]);
   let vRegex = /^[aeiou]/;
-  let noVRegex = /[aeiou]/gi;
-  // console.log(vRegex.test(str[0]));
-  // console.log(str);
   if (vRegex.test(str[0])) {
-    console.log(str + 'way')
     return str + 'way';
   } else {
       for (let i = 0; i < str.length; i++) {
         if (vRegex.test(str[i])) {
           let end = str.slice(0, i);
-          // console.log(end)
-          // console.log(str.slice(i) + end + 'ay');
           return str.slice(i) + end + 'ay';
         }
       }
@@ -20,20 +13,15 @@ function translatePigLatin(str) {
   return str + 'ay';
 }
 
-// translatePigLatin("consonant");
-// translatePigLatin("algorithm");
-// translatePigLatin("eight");
-// console.log('');
+// test cases
+translatePigLatin("consonant");
+translatePigLatin("algorithm");
+translatePigLatin("eight");
 translatePigLatin("rhythm");
-// console.log('');
 translatePigLatin("glove");
 
-console.log();
-console.log();
-console.log();
-console.log();
-
-// inputs: string
-// outputs: string
-// simple, mechanical solution
-// if (str[0] === /[aeiou]/)
+console.log(translatePigLatin("consonant"));
+console.log(translatePigLatin("algorithm"));
+console.log(translatePigLatin("eight"));
+console.log(translatePigLatin("rhythm"));
+console.log(translatePigLatin("glove"));

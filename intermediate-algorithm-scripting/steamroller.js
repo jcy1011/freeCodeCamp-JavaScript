d0b169e757cb1421b7e187124ddae892a6bc41c1
Jonathan Yale <jonathanyale@gmail.com>
2020-05-07 13:38:50 -0400

    attempted solution. Not working

--- intermediate-algorithm-scripting/steamroller.js
+++ intermediate-algorithm-scripting/steamroller.js
@@ -1,18 +1,47 @@
-// Not my solution
+// My attempt. Not working
 function steamrollArray(arr) {
-  let flattenedArr = [];
-  let flatten = function(arg) {
-    if (!Array.isArray(arg)) {
-      flattenedArr.push(arg);
+  function flatten(item) {
+    if (!Array.isArray(item)) {
+      return item
     } else {
-      for (let element in arg) {
-        flatten(arg[element]);
-      }
+      return null
     }
-  };
-  arr.forEach(flatten)
-  return flattenedArr;
+  }
+  return arr.map(flatten);
 }
+
+// steamrollArray([1, [2], [3, [[4]]]]);
+// steamrollArray([[["a"]], [["b"]]]);
+
+console.log(steamrollArray([[["a"]], [["b"]]]));
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([1, [], [3, [[4]]]]));
+console.log(steamrollArray([1, {}, [3, [[4]]]]));
+// console.log();
+
+
+// return item + ''
+// let strArr = []
+// strArr.push(item.toString())
+// console.log(strArr); 
+
+
+
+// Not my solution
+// function steamrollArray(arr) {
+//   let flattenedArr = [];
+//   let flatten = function(arg) {
+//     if (!Array.isArray(arg)) {
+//       flattenedArr.push(arg);
+//     } else {
+//       for (let element in arg) {
+//         flatten(arg[element]);
+//       }
+//     }
+//   };
+//   arr.forEach(flatten)
+//   return flattenedArr;
+// }
 
 steamrollArray([1, [2], [3, [[4]]]]);
 

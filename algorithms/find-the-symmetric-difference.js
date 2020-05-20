6fe7ab72e49495fc87213cb893745016d6599d5f
Jonathan Yale <jonathanyale@gmail.com>
2020-05-20 13:39:45 -0400

    added code the gives entries that occur once. This isn't the case of n-ary sym diff. Need to adjust.

--- algorithms/find-the-symmetric-difference.js
+++ algorithms/find-the-symmetric-difference.js
@@ -1,22 +1,42 @@
-var occurrence = function (array) {
+// This works the way I intended but I misunderstood definition of sym diff
+// see https://en.wikipedia.org/wiki/Symmetric_difference n-ary sym diff
+// the symmetric difference of a collection of sets contains just elements
+// which are in an odd number of the sets in the collection:
+const occurrence = function (array) {
   let obj = {};
   if (array instanceof Array) { // Check if input is array.
-      array.forEach(function (item, i) {
-          if (!obj[item]) { // Initial object property creation.
-              obj[item] = 1; // Create an value for that property.
-          } else { // Same occurrences found.
-              obj[item] += 1; // Increment
-          }
-      });
+    array.forEach(function (item, i) {
+        if (!obj[item]) { // Initial object property creation.
+            obj[item] = 1; // Create an value for that property.
+        } else { // Same occurrences found.
+            obj[item] += 1; // Increment
+        }
+    });
   }
   return obj;
 };
 
-console.log(occurrence([1, 2, 3].concat([5, 2, 1, 4])))
-console.log(occurrence([1, 1, 2, 5].concat([2, 2, 3, 5], [3, 4, 5, 5])))
+// console.log(occurrence([1, 2, 3].concat([5, 2, 1, 4])))
+// console.log(occurrence([1, 1, 2, 5].concat([2, 2, 3, 5], [3, 4, 5, 5])))
 
 function sym(args) {
-  return args;
+  const symDiff = [];
+  const bigArr = Array.prototype.slice.call(arguments).flat(Infinity);
+  // console.log(bigArr)
+  const entries = Object.entries(occurrence(bigArr))
+  // console.log(entries)
+  entries.forEach(arr => {
+    if (arr[1] === 1) {
+      symDiff.push(arr[0])
+    }
+  })
+  // console.log(symDiff)
+  return symDiff;
 }
+ 
+// sym([1, 2, 3], [5, 2, 1, 4]);
+// sym([1, 2, 3, 3], [5, 2, 1, 4])
+// sym([1, 2, 3], [5, 2, 1, 4, 5])
+sym([1, 2, 5], [2, 3, 5], [3, 4, 5])
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+

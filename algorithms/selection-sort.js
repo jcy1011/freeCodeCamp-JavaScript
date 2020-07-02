d2cbe2a323a70bbc0933245b8585b7387b2db8d4
Jonathan Yale <jonathanyale@gmail.com>
2020-07-02 11:57:45 -0400

    add solution

--- algorithms/selection-sort.js
+++ algorithms/selection-sort.js
@@ -3,9 +3,11 @@ function selectionSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     let min = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[min] > arr[j]) min = j;
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+      [arr[i], arr[min]] = [arr[min], arr[i]];
     }
-    [arr[i], arr[min]] = [arr[min], arr[i]];
   }
   return arr;
 }

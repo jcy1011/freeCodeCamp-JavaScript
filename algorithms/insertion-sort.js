9b452811cac2dcc902d35f9c141f342a4302e0ee
Jonathan Yale <jonathanyale@gmail.com>
2020-07-10 14:54:11 -0400

    add working sorting algorithm but of wrong type - selection

---
+++ algorithms/insertion-sort.js
@@ -0,0 +1,15 @@
+// I think this is selection sort not insertion
+
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        [arr[j],  arr[i]] = [arr[i],  arr[j]]
+      }
+    }
+  }
+  console.log(arr);
+  return arr;
+}
+
+insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
